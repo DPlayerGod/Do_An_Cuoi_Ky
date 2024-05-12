26f9e65d986a5e145efeb889c239a7490c9d71e1
Dang Quoc Cuong <150898842+DPlayerGod@users.noreply.github.com>
2024-05-12 22:28:15 +0700

    Add files via upload

---
+++ Do_An_Cuoi_Ky/BST.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// cau truc cua cay BST
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+    	
+    	// thao tac them mot phan tu
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            } else {
+                Node* cur;
+                if(data <= root->data) {
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                } else {
+                    cur = insert(root->right, data);
+                    root->right = cur;
+               }
+
+               return root;
+           }
+        }
+/*The tree node has data, left child and right child 
+class Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+*/
+	// ham tinh do cao cua cay BST
+    int height(Node* root) {
+        if (root == NULL) return 0;
+        int res = 0;
+        res = max(res, height(root->left) + 1);
+        res = max(res, height(root->right) + 1);
+        return res;
+    }
+
+}; //End of Solution
+
+int main() {
+  
+    Solution myTree;
+    Node* root = NULL;
+    
+    int t;
+    int data;
+
+    std::cin >> t;
+    // so nut t
+
+    while(t-- > 0) {
+        std::cin >> data;
+        root = myTree.insert(root, data);
+    }
+  
+    int height = myTree.height(root);
+    
+  	std::cout << height;
+
+    return 0;
+}
+
+
+
